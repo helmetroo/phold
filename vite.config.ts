c1221764817957912063dd06feca69dcbce52aa3
Samuel Sweet <samueljsweet@gmail.com>
2023-07-18 21:04:29 -0600

    - Added preliminary logo - Renamed app to PHOLD - PWA SW created in dev build - Added preliminary PWA manifest config

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,47 @@ export default defineConfig({
     plugins: [
         tsconfigPaths(),
         glsl(),
-        VitePWA(),
+        VitePWA({
+            devOptions: {
+                enabled: true,
+            },
+
+            includeAssets: [
+                'favicon.ico',
+                'apple-touch-icon.png',
+                'mask-icon.svg',
+            ],
+
+            manifest: {
+                name: 'PHOLD',
+                short_name: 'PHOLD',
+                description: 'A camera app enabling you to fold your face',
+                background_color: '#000000',
+                theme_color: '#101010',
+                icons: [{
+                    src: 'pwa-192x192.png',
+                    sizes: '192x192',
+                    type: 'image/png',
+                }, {
+                    src: 'pwa-512x512.png',
+                    sizes: '512x512',
+                    type: 'image/png',
+                }, {
+                    src: 'pwa-512x512.png',
+                    sizes: '512x512',
+                    type: 'image/png',
+                    purpose: 'any',
+                }, {
+                    src: 'pwa-512x512.png',
+                    sizes: '512x512',
+                    type: 'image/png',
+                    purpose: 'maskable',
+                }],
+                start_url: '/.',
+                display: 'standalone',
+                orientation: 'portrait',
+            }
+        }),
         preact(),
     ]
 });
