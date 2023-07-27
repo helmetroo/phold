5a35154892ac5c5ed52f5880cfafb016b2d8d999
Samuel Sweet <samueljsweet@gmail.com>
2023-07-26 19:31:17 -0600

    Drop focus state from buttons

--- src/ui/x-button.tsx
+++ src/ui/x-button.tsx
@@ -19,7 +19,7 @@ export default function XButton({ label, classes, callback }: Props) {
     return (
         <button
             aria-label={label}
-            class={`${classes || ''} ${animating && 'animate-button-press'} flex justify-center items-center rounded-[50%] w-16 h-16 bg-neutral-600 cursor-pointer transition ease-out duration-10 origin-center hover:scale-[1.1] hover:bg-neutral-900 focus:scale-[1.05] focus:bg-neutral-800`}
+            class={`${classes || ''} ${animating && 'animate-button-press'} flex justify-center items-center rounded-[50%] w-16 h-16 bg-neutral-600 cursor-pointer transition ease-out duration-10 origin-center hover:scale-[1.1] hover:bg-neutral-900`}
             onClick={onPress}
             onAnimationEnd={() => setAnimating(false)}
         >
