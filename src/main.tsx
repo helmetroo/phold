d228e55e7a177f94d8840aa6396e8e56436500e6
Samuel Sweet <samueljsweet@gmail.com>
2023-08-12 21:51:29 -0600

    Add Vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { render } from 'preact';
+import { inject } from '@vercel/analytics';
 
 import App from './app';
 import SettingsCtx, { createSettingsState } from '@/contexts/settings';
 
 import './index.css';
 
+// Analytics
+inject();
+
+// Render app
 const app = (
     <SettingsCtx.Provider value={createSettingsState()}>
         <App />
